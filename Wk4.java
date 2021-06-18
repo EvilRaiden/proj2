6cbd6e5d5702a50e1450f29d49079d3120df7df7
EvilRaiden <78463694+EvilRaiden@users.noreply.github.com>
2021-06-18 15:34:27 +0530

    Add files via upload
    file 1 added by iresh

---
+++ Wk4.java
@@ -0,0 +1,33 @@
+class Wk4{
+	
+	public static void main(String[] args){
+		
+		double ans= multiplyNumbers();
+		System.out.println("Result is : "+ans);		
+	}
+
+	public static double multiplyNumbers(double num1, double num2){
+		double ans = num1*num2;
+		return ans;	
+	}
+
+	public static double multiplyNumbers(double num1, double num2, double num3){
+		double ans = num1*num2*num3;
+		return ans;
+	}
+
+	public static double multiplyNumbers(double num1, int num2){
+		double ans = num1*num2;
+		return ans;
+	}
+
+	public static double multiplyNumbers(){
+		return 9999.9999;
+	}
+
+	public static int multiplyIntNumbers(){
+		return 10;
+	}
+
+
+}
